6a66e93b75b83dd0057d28b847b415421b6fe688
P-marcou <pmarcou@outlook.com>
2016-06-21 15:36:05 +0200

    Models
    Article
    Utilisateur
    Commande

--- src/model/Article.java
+++ src/model/Article.java
@@ -1,5 +1,45 @@
 package model;
 
 public class Article {
-
+	
+	private int identifiant;
+	private int libelle;
+	private int poids;
+	
+	public Article(int libelle, int poids) {
+		super();
+		this.libelle = libelle;
+		this.poids = poids;
+	}
+	
+	public Article(int identifiant, int libelle, int poids) {
+		super();
+		this.identifiant = identifiant;
+		this.libelle = libelle;
+		this.poids = poids;
+	}
+	
+	public int getIdentifiant() {
+		return identifiant;
+	}
+	
+	public void setIdentifiant(int identifiant) {
+		this.identifiant = identifiant;
+	}
+	
+	public int getLibelle() {
+		return libelle;
+	}
+	
+	public void setLibelle(int libelle) {
+		this.libelle = libelle;
+	}
+	
+	public int getPoids() {
+		return poids;
+	}
+	
+	public void setPoids(int poids) {
+		this.poids = poids;
+	}
 }
--- src/model/Utilisateur.java
+++ src/model/Utilisateur.java
@@ -2,4 +2,55 @@ package model;
 
 public class Utilisateur {
 
+	private int identifiant;
+	private String nom;
+	private String login;
+	private String motDePasse;
+	
+	public Utilisateur(String nom, String login, String motDePasse) {
+		super();
+		this.nom = nom;
+		this.login = login;
+		this.motDePasse = motDePasse;
+	}
+
+	public Utilisateur(int identifiant, String nom, String login, String motDePasse) {
+		super();
+		this.identifiant = identifiant;
+		this.nom = nom;
+		this.login = login;
+		this.motDePasse = motDePasse;
+	}
+
+	public int getIdentifiant() {
+		return identifiant;
+	}
+	
+	public void setIdentifiant(int identifiant) {
+		this.identifiant = identifiant;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+	
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
 }
