980d9369d12d6076e1a37d56cf1f11fcb09b1067
Greg <d1412waltong@12-uc29-21.ad.campus-eni.fr>
2016-06-22 10:03:07 +0200

    Etat

---
+++ src/util/Utilities.java
@@ -0,0 +1,22 @@
+package util;
+
+import model.Etats;
+
+public class Utilities {
+
+	public static Etats getEtat(int numEtat){
+		switch (numEtat) {
+		case 0:
+			return Etats.AFFECTABLE;
+		case 1:
+			return Etats.AFFECTEE;
+		case 2:
+			return Etats.EXPEDIEE;			
+		case 3:
+			return Etats.REAFFECTABLE;
+		default:
+			return null;
+		}
+	}
+
+}
