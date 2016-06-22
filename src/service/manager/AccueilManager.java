a322952aa901d3e33640226f1ab62b07e8737f5b
Greg <d1412waltong@12-uc29-21.ad.campus-eni.fr>
2016-06-22 14:48:39 +0200

    Tous les Servlets

--- src/service/manager/AccueilManager.java
+++ src/service/manager/AccueilManager.java
@@ -1,11 +1,17 @@
 package service.manager;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dto.LigneCommandeManager;
 
 /**
  * Servlet implementation class AccueilManager
@@ -27,7 +33,10 @@ public class AccueilManager extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		List<LigneCommandeManager> commandes = new ArrayList<LigneCommandeManager>();//DAL
+		request.setAttribute("commandes", commandes);
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/Expeditor/WebContent/web/jsp/employe/employe.jsp");
+		requestDispatcher.forward(request, response);
 	}
 
 	/**
