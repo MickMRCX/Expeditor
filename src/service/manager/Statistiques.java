3099a4b82091465fdc0037a74a38d0fc68081ec3
d1412duvalm <d1412duvalm@12-uc29-29.ad.campus-eni.fr>
2016-06-23 14:39:10 +0200

    Fic servlet staistiques

--- src/service/manager/Statistiques.java
+++ src/service/manager/Statistiques.java
@@ -36,7 +36,7 @@ public class Statistiques extends HttpServlet {
 		ICommandeDAL commandeDAL = DALFactory.getCommandeDAL();
 		dto.Statistiques statistiques = commandeDAL.getStatistiques();
 		request.setAttribute("statistiques", statistiques);
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/Expeditor/WebContent/web/jsp/employe/employe.jsp");
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/web/jsp/employe/employe.jsp");
 		requestDispatcher.forward(request, response);
 		
 	}
