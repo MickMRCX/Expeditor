02b67033438f5f4c324554d04dc9c06a8b255f76
Greg <d1412waltong@12-uc29-21.ad.campus-eni.fr>
2016-06-23 15:30:58 +0200

    requete employes

--- src/dal/impl/UtilisateurDAL.java
+++ src/dal/impl/UtilisateurDAL.java
@@ -34,7 +34,7 @@ public class UtilisateurDAL implements IUtilisateurDAL {
 
 	private final String DELETE = "DELETE FROM Utilisateurs WHERE Identifiant = ?";
 	
-	private final String SELECT_EMPLOYES = "SELECT Identifiant, Nom, Login, MotDePasse FROM Utilisateurs u INNER JOIN Droits d ON u.Login = d.Login";
+	private final String SELECT_EMPLOYES = "SELECT Identifiant, Nom, Login, MotDePasse FROM Utilisateurs u INNER JOIN Droits d ON u.Login = d.Login WHERE d.Libelle = 'employe'";
 
 	
 	UtilisateurDAL() {
